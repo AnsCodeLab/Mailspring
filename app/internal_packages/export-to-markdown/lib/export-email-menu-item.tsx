91aaed92a97d467f859089aff0898f201a76146a
AnsCodeLab <annguyen209@gmail.com>
2026-06-15 10:36:47 +0700

    feat: add ExportEmailMenuItem for per-message export

---
+++ app/internal_packages/export-to-markdown/lib/export-email-menu-item.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Message, Thread, DatabaseStore, localized } from 'mailspring-exports';
+import { buildSingleMessageMarkdown, saveMarkdownFile } from './export-utils';
+
+export default class ExportEmailMenuItem extends React.Component {
+  static displayName = 'ExportEmailMenuItem';
+
+  static getMenuItem({ message, thread }: { message: Message; thread: Thread }) {
+    return {
+      label: localized('Export as Markdown'),
+      click: async () => {
+        let msg = message;
+        if (!msg.body) {
+          msg = await DatabaseStore.find<Message>(Message, message.id).include(
+            Message.attributes.body
+          );
+          if (!msg) return;
+        }
+        const content = buildSingleMessageMarkdown(msg);
+        await saveMarkdownFile(content, msg.subject || thread.subject);
+      },
+    };
+  }
+
+  render() {
+    return null;
+  }
+}
